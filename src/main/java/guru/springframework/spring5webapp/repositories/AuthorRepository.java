340ffe51be0e9763ab6a3f60e06a28955741bdc0
GrAM0Z <john198603@gmail.com>
2022-06-26 22:50:38 +0500

    lesson 17 Initializing Data with Spring

--- src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
+++ src/main/java/guru/springframework/spring5webapp/repositories/AuthorRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Gk5
  * Date: 2022-06-26
  */
-public interface AutorRepository extends CrudRepository<Author, Long> {
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
